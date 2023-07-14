8d2dab7f270abfd0a2ef7f5d052c410679c5699e
gordey1313 <gordey_2000@yahoo.com>
2023-07-14 13:37:24 +0300

    Sort published_at with nulls last

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,7 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
     
-    scope :sorted, -> { order(published_at: :desc, updated_at: :desc)}
+    scope :sorted, -> { order(arel_table[:published_at].desc.nulls_last).order(updated_at: :desc)}
     scope :draft, -> { where(published_at: nil)}
     scope :published, -> { where("published_at <= ?", Date.current)}
     scope :scheduled, -> { where("published_at > ?", Date.current)}
@@ -19,7 +19,7 @@ class Article < ApplicationRecord
         published_at? && published_at <= Date.current
     end
 
-    def schedule?
+    def scheduled?
         published_at? && published_at > Date.current
     end
 
